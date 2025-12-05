f00430b52ef390e4787fb5445a4d54eec02646cd
mohitmonash <msha0242@student.monash.edu>
2025-12-05 23:37:39 +1100

    Initial Commit

---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Mohit Sharma on 5/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
